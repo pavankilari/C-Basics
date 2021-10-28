5e221ccef6228809ce16a38dcc1ff51b49e041c5
pavankilari <92910467+pavankilari@users.noreply.github.com>
2021-10-28 00:03:39 -0700

    Add files via upload

---
+++ DOUBLE QUOTE.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+void main()
+{
+	printf("MY EXPERIMENTS WITH TRUTH is written by \"MAHATMA GANDHI\"");
+}
---
+++ funcin of backslash r.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+void main()
+{
+	printf("How\rare\rYou");
+}
